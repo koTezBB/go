bb0597131e0b9d0784a1f39551f9c0ce859602b2
Vitali Kotkovets <kotkovetz22@gmail.com>
2022-12-21 12:40:40 +0300

    create page
    *create home page
    *create contacts page

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "go nice")
+}
+
+func contacts_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "contacts page")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/contacts/", contacts_page)
+
+	http.ListenAndServe(":8080", nil)
+}
 
 func main() {
-	fmt.Println("Go Top")
+	handleRequests()
+
 }
